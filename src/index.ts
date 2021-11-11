11289451d661dcdabff3274c33c89ff3eb844b4c
Vladislav Mamon <norskeld@gmail.com>
2021-11-11 19:59:26 +0500

    refactor(tests): refactor, improve & restructure to mirror `src`
    - Add test for public API.
    - Clean up re-exports.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './internal/state'
 
-// Re-export for convenience.
-export { string, str } from './combinators'
+// Re-export combinators for convenience.
+export * from './combinators'
--- tests/@setup/jest.helpers.ts
+++ tests/@setup/jest.helpers.ts
@@ -7,3 +7,7 @@ export function run<T>(parser: Parser<T>, input: string): Result<T> {
 export function result<T>(kind: 'success' | 'failure', value: T) {
   return { kind, value } as const
 }
+
+export function shouldExpose(exposed: Record<string, unknown>, ...exposees: Array<string>): void {
+  exposees.forEach((exposee) => expect(exposed).toHaveProperty(exposee))
+}
