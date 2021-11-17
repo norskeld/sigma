12b911677c85ee8d1aa716ea0d07bb8cb7ad1b6c
Vladislav Mamon <norskeld@gmail.com>
2021-11-17 16:49:34 +0500

    test(unicode): test `[E000, FFFF]` range

--- tests/internal/unicode.spec.ts
+++ tests/internal/unicode.spec.ts
@@ -10,7 +10,7 @@ describe(size, () => {
       : expect(size(pangram.text)).toBe(pangram.size)
   }
 
-  it('should correctly count size of unicode pangrams in bytes', () => {
+  it('should correctly get length of unicode pangrams in bytes', () => {
     check('da')
     check('en')
     check('fr')
@@ -25,11 +25,30 @@ describe(size, () => {
     check('es')
   })
 
-  it('should correctly count size of emoji in bytes', () => {
+  it('should correctly get length of emojis in bytes', () => {
     expect(size(`⚡`)).toBe(3)
     expect(size(`🦶🏿`)).toBe(8)
     expect(size(`👨‍👨‍👧‍👧`)).toBe(25)
     expect(size(`🏴󠁧󠁢󠁥󠁮󠁧󠁿`)).toBe(28)
+  })
+
+  it('should correctly calculate length of any char in bytes in range [E000, FFFF]', () => {
+    ;[
+      '\uE000', // Private Use Area
+      '\uF900', // CJK Compatibility Ideographs
+      '\uFB00', // Alphabetic Presentation Forms
+      '\uFB50', // Arabic Presentation Forms
+      '\uFE00', // Variation Selectors
+      '\uFE10', // Vertical Forms
+      '\uFE20', // Combining Half Marks
+      '\uFE30', // CJK Compatibility Forms
+      '\uFE50', // Small Form Variants
+      '\uFE70', // Arabic Presentation Forms-B
+      '\uFF00', // Half-Width and Fullwidth Forms
+      '\uFFF0' // Specials
+    ].forEach((char) => {
+      expect(size(char)).toBe(3)
+    })
   })
 
   it('should throw if provided with invalid single surrogates', () => {
@@ -41,8 +60,8 @@ describe(size, () => {
       '\uDC00', // Invalid high surrogate
       '\uDF80', // Invalid high surrogate
       '\uDFFF' // Invalid high surrogate
-    ].forEach((single) => {
-      expect(() => size(single)).toThrow()
+    ].forEach((char) => {
+      expect(() => size(char)).toThrow()
     })
   })
 })
