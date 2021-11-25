1a82c7684edb95a0fe5ea138e29779d9848d5950
Vladislav Mamon <norskeld@gmail.com>
2021-11-25 13:14:42 +0500

    refactor(parsers/rest): refactor a bit

--- src/internal/parsers/rest.ts
+++ src/internal/parsers/rest.ts
@@ -3,14 +3,13 @@ import { success, State, Parser } from '../state'
 export function rest(): Parser<string> {
   return {
     parse(state: State) {
-      const result = state.text.substring(state.index)
-
-      const nextState: State = {
-        text: state.text,
-        index: state.text.length
-      }
-
-      return success(nextState, result)
+      return success(
+        {
+          text: state.text,
+          index: state.text.length
+        },
+        state.text.substring(state.index)
+      )
     }
   }
 }
