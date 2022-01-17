e1a685685a0d1b215f1a28ac3c6b6c557c6e145a
Vladislav Mamon <norskeld@gmail.com>
2022-01-17 17:56:44 +0500

    docs: visual adjustments & improvements

--- docs/src/config/seo.ts
+++ docs/src/config/seo.ts
@@ -15,7 +15,7 @@ export const Common = {
 /** Default SEO options/properties that are passed to `next-seo` and universal across all pages. */
 export const Seo: DefaultSeoProps = {
   defaultTitle: Common.title,
-  titleTemplate: `%s | ${Common.title}`,
+  titleTemplate: `${Common.title} :: %s `,
   description: Common.description,
   openGraph: {
     type: 'website',
