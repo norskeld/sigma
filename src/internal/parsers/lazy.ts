33fe195b729fbb2610b4bc84d7ecc014f5f70f94
Vladislav Mamon <norskeld@gmail.com>
2022-01-22 17:07:42 +0500

    refactor(parsers/lazy)!: remove `lazy` parser
    The `defer` parser is superior to it at every aspect, so getting rid of it.

--- src/internal/parsers/lazy.ts
+++
@@ -1,9 +0,0 @@
-import { State, Parser } from '../state'
-
-export function lazy<T>(thunk: () => Parser<T>): Parser<T> {
-  return {
-    parse(state: State) {
-      return thunk().parse(state)
-    }
-  }
-}
