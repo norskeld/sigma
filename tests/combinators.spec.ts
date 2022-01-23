529eed075a02be4f15d380b83aa7fca8f795a1a3
Vladislav Mamon <norskeld@gmail.com>
2022-01-23 07:37:05 +0500

    refactor: re-export everything in index.ts

--- tests/combinators.spec.ts
+++ tests/combinators.spec.ts
@@ -2,21 +2,22 @@ import * as exposed from '@lib/combinators'
 
 import { should } from '@tests/@helpers'
 
+export const expectedCombinators = [
+  'chainl',
+  'choice',
+  'error',
+  'sepBy',
+  'many',
+  'map',
+  'mapTo',
+  'optional',
+  'sequence',
+  'takeLeft',
+  'takeMid',
+  'takeRight',
+  'takeSides'
+] as const
+
 it('should expose combinators', () => {
-  should.expose(
-    exposed,
-    'chainl',
-    'choice',
-    'error',
-    'sepBy',
-    'many',
-    'map',
-    'mapTo',
-    'optional',
-    'sequence',
-    'takeLeft',
-    'takeMid',
-    'takeRight',
-    'takeSides'
-  )
+  should.expose(exposed, ...expectedCombinators)
 })
