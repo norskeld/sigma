323344035537f3d8ece23ef6826a0a764b78f89e
Vladislav Mamon <norskeld@gmail.com>
2022-07-12 23:11:46 +0500

    feat(tsdoc): final touches

--- src/parsers/run.ts
+++ src/parsers/run.ts
@@ -1,8 +1,6 @@
 import type { Parser, Result } from '../state'
 
-/**
- * @internal
- */
+/** @internal */
 interface Runnable<T> {
   with(input: string): Result<T>
 }
@@ -10,7 +8,6 @@ interface Runnable<T> {
 /**
  * Runs a parser with provided input.
  *
- * @typeParam T - Type of parser's result
  * @param parser - Parser to run
  *
  * @returns Parser result
