d9a54a86f8f8856b7c76c3385df86dbdbcaa75e7
Danil Tarasov <danil.tarasov@itechart-group.com>
2022-10-08 15:58:07 +0300

    feat(build): finalize tsup setup with multiple options for build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,31 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, type Options } from 'tsup'
 
 const entry = ['src/index.ts', 'src/parsers.ts', 'src/combinators.ts']
 
-export default defineConfig({
-  entry,
+const sharedConfig = defineConfig({
   splitting: false,
   sourcemap: true,
   clean: true,
   format: ['esm', 'cjs'],
   treeshake: true,
-  dts: true,
   minify: false,
   bundle: true
 })
+
+const mainConfig = defineConfig({
+  ...sharedConfig,
+  entry,
+  dts: false
+}) as Options
+
+const createDTSConfig = (e: string): Options =>
+  defineConfig({
+    ...sharedConfig,
+    entry,
+    dts: {
+      entry: e,
+      only: true
+    }
+  }) as Options
+
+export default defineConfig([mainConfig, ...entry.map(createDTSConfig)])
