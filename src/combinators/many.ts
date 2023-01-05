4630bf4e53749b91efb906be8ece5429aa74eddb
Vladislav Mamon <norskeld@gmail.com>
2023-01-05 11:10:21 +0300

    fix(combinators/many): fix an edge case when combined with `noneOf`
    Seems more like an issue with `noneOf`, which may go out of the input bounds, but `many` and `many1` needed a fix as well.

--- src/combinators/many.ts
+++ src/combinators/many.ts
@@ -13,24 +13,21 @@ export function many<T>(parser: Parser<T>): SafeParser<Array<T>> {
       const values: Array<T> = []
       let nextPos = pos
 
-      while (true) {
+      while (nextPos < input.length) {
         const result = parser.parse(input, nextPos)
 
-        switch (result.isOk) {
-          case true: {
-            values.push(result.value)
-            nextPos = result.pos
-            break
-          }
-
-          case false: {
-            return {
-              isOk: true,
-              pos: nextPos,
-              value: values
-            }
-          }
+        if (result.isOk) {
+          values.push(result.value)
+          nextPos = result.pos
+        } else {
+          break
         }
+      }
+
+      return {
+        isOk: true,
+        pos: nextPos,
+        value: values
       }
     }
   }
@@ -54,7 +51,7 @@ export function many1<T>(parser: Parser<T>): Parser<Array<T>> {
 
         values.push(resultP.value)
 
-        while (true) {
+        while (nextPos < input.length) {
           const resultR = parser.parse(input, nextPos)
 
           if (resultR.isOk) {
@@ -63,11 +60,13 @@ export function many1<T>(parser: Parser<T>): Parser<Array<T>> {
             continue
           }
 
-          return {
-            isOk: true,
-            pos: nextPos,
-            value: values
-          }
+          break
+        }
+
+        return {
+          isOk: true,
+          pos: nextPos,
+          value: values
         }
       }
 
