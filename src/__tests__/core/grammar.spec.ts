2ef5dfee754f1fbedf0758b6615ded9a91b65bc2
Rasmus Schultz <mindplay-dk@users.noreply.github.com>
2023-08-20 10:29:29 +0200

    Update src/__tests__/core/grammar.spec.ts
    Co-authored-by: Vladislav Mamon <norskeld@gmail.com>

--- src/__tests__/core/grammar.spec.ts
+++ src/__tests__/core/grammar.spec.ts
@@ -1,24 +1,23 @@
-import { grammar, run } from '@core'
-import { Parser, Success, choice, map, optional, sequence, string } from '@lib'
-import { describe, should, it } from '@testing'
+import { grammar } from '@core'
+import { choice, map, optional, sequence, string } from '@lib'
+import { describe, should, it, run, result } from '@testing'
 
-describe(`grammar`, () => {
+describe('grammar', () => {
   it('should create self-contained grammar', () => {
     const ab = grammar({
-      a(): Parser<string> {
-        return map(sequence(string('a'), optional(this.b)), ([a, b]) => a + (b || ''))
+      a() {
+        return map(sequence(string('a'), optional(this.b)), ([a, b]) => a + (b ?? ''))
       },
-      b(): Parser<string> {
+      b() {
         return map(sequence(string('b'), choice(this.a, this.b)), ([b, a]) => b + a)
       }
     })
 
     const value = 'abba'
 
-    const actual = run(ab.a).with(value)
+    const actual = run(ab.a, value)
+    const expected = result(true, value)
 
-    const expected: Success<string> = { isOk: true, value, span: [0, 4], pos: 4 }
-
-    should.beStrictEqual(actual, expected)
+    should.matchState(actual, expected)
   })
 })
