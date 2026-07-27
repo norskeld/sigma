41c30668842be7fa11b8ee8a2cf6d245aebbe74d
Vladislav Mamon <norskeld@gmail.com>
2026-07-27 04:23:17 +0300

    Minor pnpm tweaks, bump tsdown version

--- packages/sigma/tsdown.config.ts
+++ packages/sigma/tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  target: 'es2022',
+  target: 'es2025',
   sourcemap: true,
   treeshake: true,
   clean: true,
